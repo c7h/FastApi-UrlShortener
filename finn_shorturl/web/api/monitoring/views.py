352e6f601235debe79eee0478e6c1096ccd20390
Christoph Gerneth <christoph@gerneth.info>
2023-12-07 23:27:03 +0100

    health endpoint response models

--- finn_shorturl/web/api/monitoring/views.py
+++ finn_shorturl/web/api/monitoring/views.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter
+from .schemas import HealthResponse
 
 router = APIRouter()
 
 
-@router.get("/health")
+@router.get("/health", response_model=HealthResponse)
 def health_check() -> None:
     """
     Checks the health of a project.
 
     It returns 200 if the project is healthy.
     """
-    return {"message": "OK"}
+    return HealthResponse(message="OK")
 
